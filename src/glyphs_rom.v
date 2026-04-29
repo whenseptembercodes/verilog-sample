96bcefb548f63d2d84c0af3f5cefc7dbee596d2e
whenseptembercodes <upborne.s@outlook.com>
2026-04-29 16:22:20 +0800

    Update glyphs_rom.v

--- src/glyphs_rom.v
+++ src/glyphs_rom.v
@@ -8,19 +8,21 @@ module glyphs_rom(
     always @(*) begin
         case (c) 
             0:  case(y) 2:rb=8'h3C; 3,4,5:rb=8'h66; 6,7:rb=8'hFF; 8,9,10:rb=8'hC3; default:rb=0; endcase // A
-            1:  case(y) 2,6,10:rb=8'hFC; 3,4,5,7,8,9:rb=8'hC6; default:rb=0; endcase // B
+            /*1:  case(y) 2,6,10:rb=8'hFC; 3,4,5,7,8,9:rb=8'hC6; default:rb=0; endcase // B
             2:  case(y) 2,10:rb=8'h7E; 3,9:rb=8'hC3; 4,5,6,7,8:rb=8'hC0; default:rb=0; endcase // C
             3:  case(y) 2,10:rb=8'hF8; 3,4,5,6,7,8,9:rb=8'hC6; default:rb=0; endcase // D
             4:  case(y) 2,6,10:rb=8'hFE; 3,4,5,7,8,9:rb=8'hC0; default:rb=0; endcase // E
             5:  case(y) 2,6:rb=8'hFE; 3,4,5,7,8,9,10:rb=8'hC0; default:rb=0; endcase // F
             6:  case(y) 2,10:rb=8'h7E; 3,4,5:rb=8'hC0; 6:rb=8'hCE; 7,8,9:rb=8'hC6; default:rb=0; endcase // G
-            7:  case(y) 2,3,4,5,7,8,9,10:rb=8'hC6; 6:rb=8'hFE; default:rb=0; endcase // H
-            8:  case(y) 2,10:rb=8'h7E; 3,4,5,6,7,8,9:rb=8'h18; default:rb=0; endcase // I
-            9:  case(y) 2:rb=8'h3F; 3,4,5,6,7,8:rb=8'h0C; 9:rb=8'hCC; 10:rb=8'h78; default:rb=0; endcase // J
+            7:  case(y) 2,3,4,5,7,8,9,10:rb=8'hC6; 6:rb=8'hFE; default:rb=0; endcase // H*/
+            //8:  case(y) 2,10:rb=8'h7E; 3,4,5,6,7,8,9:rb=8'h18; default:rb=0; endcase // I
+            1:  case(y) 2,10:rb=8'h7E; 3,4,5,6,7,8,9:rb=8'h18; default:rb=0; endcase // I
+            /*9:  case(y) 2:rb=8'h3F; 3,4,5,6,7,8:rb=8'h0C; 9:rb=8'hCC; 10:rb=8'h78; default:rb=0; endcase // J
             10: case(y) 2,10:rb=8'hC6; 3,9:rb=8'hCC; 4,8:rb=8'hD8; 5,7:rb=8'hF0; 6:rb=8'hE0; default:rb=0; endcase // K
-            11: case(y) 2,3,4,5,6,7,8,9:rb=8'hC0; 10:rb=8'hFE; default:rb=0; endcase // L
-            12: case(y) 2:rb=8'hC3; 3:rb=8'hE7; 4:rb=8'hFF; 5:rb=8'hDB; 6,7,8,9,10:rb=8'hC3; default:rb=0; endcase // M
-            13: case(y) 2,3:rb=8'hC6; 4:rb=8'hE6; 5:rb=8'hF6; 6:rb=8'hD6; 7:rb=8'hC6; 8:rb=8'hCE; 9,10:rb=8'hC6; default:rb=0; endcase // N
+            11: case(y) 2,3,4,5,6,7,8,9:rb=8'hC0; 10:rb=8'hFE; default:rb=0; endcase // L*/
+            //12: case(y) 2:rb=8'hC3; 3:rb=8'hE7; 4:rb=8'hFF; 5:rb=8'hDB; 6,7,8,9,10:rb=8'hC3; default:rb=0; endcase // M
+            2: case(y) 2:rb=8'hC3; 3:rb=8'hE7; 4:rb=8'hFF; 5:rb=8'hDB; 6,7,8,9,10:rb=8'hC3; default:rb=0; endcase // M
+            /*13: case(y) 2,3:rb=8'hC6; 4:rb=8'hE6; 5:rb=8'hF6; 6:rb=8'hD6; 7:rb=8'hC6; 8:rb=8'hCE; 9,10:rb=8'hC6; default:rb=0; endcase // N
             14: case(y) 2,10:rb=8'h3C; 3,9:rb=8'h66; 4,5,6,7,8:rb=8'hC3; default:rb=0; endcase // O
             15: case(y) 2,6:rb=8'hFC; 3,4,5:rb=8'hC6; 7,8,9,10:rb=8'hC0; default:rb=0; endcase // P
             16: case(y) 2,10:rb=8'h3C; 3,9:rb=8'h66; 4,5,6,7:rb=8'hC3; 8:rb=8'hD3; 9:rb=8'h66; 11:rb=8'h0C; default:rb=0; endcase // Q
@@ -43,7 +45,7 @@ module glyphs_rom(
             33: case(y) 2:rb=8'h3E; 3,4:rb=8'h60; 5:rb=8'h7C; 6,7,8,9:rb=8'h66; 10:rb=8'h3C; default:rb=0; endcase // 6
             34: case(y) 2:rb=8'hFF; 3:rb=8'h06; 4:rb=8'h0C; 5:rb=8'h18; 6,7,8,9,10:rb=8'h18; default:rb=0; endcase // 7
             35: case(y) 2,6,10:rb=8'h7E; 3,4,5,7,8,9:rb=8'h66; default:rb=0; endcase // 8
-            36: case(y) 2,6,10:rb=8'h7E; 3,4,5:rb=8'h66; 7,8,9:rb=8'h06; default:rb=0; endcase // 9
+            36: case(y) 2,6,10:rb=8'h7E; 3,4,5:rb=8'h66; 7,8,9:rb=8'h06; default:rb=0; endcase // 9*/
             37: rb = 8'h00; // TRAILING SPACE
             default: rb = 8'h00; 
         endcase
